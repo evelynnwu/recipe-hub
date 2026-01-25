be17a216d952ffe92b93df9b978222e0b4e48f24
Evelyn Wu <evelynn.wu12@gmail.com>
2026-01-24 21:56:12 -0500

    removed aws-sam files

--- .aws-sam/build/ParseRecipeFunction/recipe_scrapers/farmhouseonboone.py
+++
@@ -1,8 +0,0 @@
-# mypy: disallow_untyped_defs=False
-from ._abstract import AbstractScraper
-
-
-class FarmhouseOnBoone(AbstractScraper):
-    @classmethod
-    def host(cls):
-        return "farmhouseonboone.com"
